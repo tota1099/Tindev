3104c1960d9ff465cd9056926e9ad4c0a6e1be18
Renan Porto <renan.porto1099@gmail.com>
2019-08-17 16:11:26 -0300

    First Commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
+
+mongose.connect('mongodb+srv://omnistack:omnistack@cluster0-btyk0.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
